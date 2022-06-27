648d7ade3fe4737057035fee4d66ef78f71055b5
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-06-27 13:09:12 -0500

    Traducción 1.00

--- static/js/index.js
+++ static/js/index.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
     console.log('Document is Ready')
 
-    //  getting the date using Date() object and converting it to a string
+    //  Obteniendo la fecha usando el objeto Date() y convirtiéndolos a cadena de caracteres
     let date = new Date()
     let current_date = date.toDateString()
 
-    //  display the date on the HTML page using JQUERY and JS
-    $('#date').text('Date : ' + current_date)
+    //  Mostrar la fecha en la página HTML usando JQUERY y JS
+    $('#date').text('Fecha: ' + current_date)
 
     
     let review = ""
@@ -16,34 +16,34 @@ $(document).ready(function(){
     let emotion = ""
     let emoji_url = ""
 
-    //  making a function for AJAX request
+    //  Haciendo una función para una solicitud AJAX
     function ajax_request(api_url , input_data){
 
         $.ajax({
 
-            // type of request
+            // Tipo de solicitud
             type : 'POST',
 
-            // url
+            // URL
             url : api_url,
 
-            //  JSON data
+            //  Datos JSON
             data : JSON.stringify(input_data),
 
-            //  datatype of expected response
+            //  Tipo de dato de la respuesta esperada
             dataType : 'json',
 
-            //  content type
+            //  Tipo de contenido
             contentType : 'application/json',
 
-            //  success method
+            //  Método "success"
             success : function(result)
             {
-                //  extract the sentiment and emoji path
+                //  Extraer la ruta de los sentimientos y emoticonos
                 emotion = result.sentiment
                 emoji_url = result.path
 
-                //  update the emoticon and sentiment accordingly
+                //  Actualizar el emoticón y los sentiminetos según corresponda
                 if (product  ==  'Smartphone'){
                     $('#m_emoji').attr('src' , emoji_url)
                     $('#m_emotion').text(emotion)
@@ -73,7 +73,7 @@ $(document).ready(function(){
                 }
             },
 
-            //  error method
+            //  Método "error"
             error : function(result)
             {
                 console.log(result)
@@ -81,12 +81,12 @@ $(document).ready(function(){
 
         })
 
-        console.log('ajax request sent')
+        console.log('Solicitud AJAX enviada')
         
     }
 
 
-    //  check if Submit button under 'smartphone' is clicked and get the review accordingly
+    //  Verificar si se hace clic en el botón para enviar debajo de "smartphone" y obtener la reseña correspondiente
    
 
 
